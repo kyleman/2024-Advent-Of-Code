cb8355a8e145bd30c7df238da0c992aa35613c2e
Kyle Borah <kyleborah1234321@gmail.com>
2024-12-08 14:40:50 -0600

    Add file reading so I don't have to keep adding it

--- src/main/java/com/AOC/AdventOfCode.java
+++ src/main/java/com/AOC/AdventOfCode.java
@@ -1,7 +1,19 @@
 package com.AOC;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Scanner;
+
 public class AdventOfCode {
 	public static void main(String[] args) {
-		// code here
+		String fileName = "filename.txt";
+		try (Scanner reader = new Scanner(new File(fileName))) {
+			while (reader.hasNextLine()) {
+				System.out.println(reader.nextLine());
+			}
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("File not found: " + fnfe);
+		}
 	}
 }
